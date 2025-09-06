822010164cacd57bc2616e8c7a12bfb1e0a2be54
AClon314 <30747832+AClon314@users.noreply.github.com>
2025-09-06 14:55:45 +0800

    format: black formatter

--- src/mocap_wrapper/app.py
+++ src/mocap_wrapper/app.py
@@ -2,6 +2,7 @@
 import os
 import logging
 from warnings import filterwarnings
+
 SELF_DIR = os.path.dirname(os.path.abspath(__file__))
 filterwarnings("ignore", category=RuntimeWarning)
 filterwarnings("ignore", category=DeprecationWarning)
@@ -14,59 +15,113 @@ import copy
 import asyncio
 import argparse
 from typing import Sequence
-from .lib import getLogger, ffmpeg_or_link, Python, CONFIG, PACKAGE, QRCODE, RUNS, TYPE_RUNS, VERSION
+from .lib import (
+    getLogger,
+    ffmpeg_or_link,
+    Python,
+    CONFIG,
+    PACKAGE,
+    QRCODE,
+    RUNS,
+    TYPE_RUNS,
+    VERSION,
+)
 from .install.static import install
-DEFAULT: Sequence[TYPE_RUNS] = ('wilor', 'gvhmr')
-OUTPUT_DIR = os.path.join(CONFIG['search_dir'], 'output')
-def version(): return f'{PACKAGE} {VERSION} 👻\tconfig: {CONFIG.path}\tcode: https://github.com/AClon314/mocap-wrapper'
-async def gather(*args, **kwargs): return await asyncio.gather(*args, **kwargs)
+
+DEFAULT: Sequence[TYPE_RUNS] = ("wilor", "gvhmr")
+OUTPUT_DIR = os.path.join(CONFIG["search_dir"], "output")
+
+
+def version():
+    return f"{PACKAGE} {VERSION} 👻\tconfig: {CONFIG.path}\tcode: https://github.com/AClon314/mocap-wrapper"
+
+
+async def gather(*args, **kwargs):
+    return await asyncio.gather(*args, **kwargs)
+
+
 _VERSION_ = version()
 Log = getLogger(__name__)
 
 
-async def run(runs: Sequence[TYPE_RUNS], input: str, outdir: str, Range='', args: Sequence[str] = []):
-    video = await ffmpeg_or_link(input, outdir, Range=Range)
+async def run(
+    runs: Sequence[TYPE_RUNS],
+    input: str,
+    outdir: str,
+    Range="",
+    args: Sequence[str] = [],
+):
+    video = await ffmpeg_or_link(input, outdir, Range=Range) if input else ""
     for m in runs:
-        p = await Python('--input', video, '-o', outdir, *args, run=m)
+        p = await Python("--input", video, "-o", outdir, *args, run=m)
 
 
 class ArgParser(argparse.ArgumentParser):
     def print_help(self, file=None):
         print(QRCODE)
-        print(f'example: mocap -I -@ .. -i input.mp4')
+        print(f"example: mocap -I -@ .. -i input.mp4")
         super().print_help(file)
-        tasks = [Python('--help', run=m)for m in DEFAULT]
+        tasks = [Python("--help", run=m) for m in DEFAULT]
         asyncio.run(gather(*tasks))
 
 
 def argParse():
     print(_VERSION_)
-    arg = ArgParser(description=f'sincerelly thanks to gvhmr/wilor/wilor-mini devs and others that help each other♥️ , please consider donate♥️ if helps you a lot :)')
-    arg.add_argument('-v', '--version', action='store_true')
-    arg.add_argument('-I', '--install', action='store_true', help='force to re-install runs')
-    arg.add_argument('-b', '--by', nargs='*', default=False, metavar=','.join(RUNS), help=f'install with/run by, default all installed, eg: `--by={",".join(DEFAULT)}`')
-    arg.add_argument('-@', '--at', metavar=CONFIG['search_dir'], help='search_dir of git repos, eg: `--at=".."` if GVHMR is current work dir')
-    arg.add_argument('-i', '--input', nargs='*', metavar='in.mp4')
-    arg.add_argument('-o', '--outdir', metavar=OUTPUT_DIR, default=OUTPUT_DIR)
-    arg.add_argument('-r', '--range', metavar='[a,b]or[a,duration]', default='', help='video time range, eg: `--range=0:0:1,0:2` is 1s~2s, `--range=10` is 0s~10s')
+    arg = ArgParser(
+        description=f"sincerelly thanks to gvhmr/wilor/wilor-mini devs and others that help each other♥️ , please consider donate♥️ if helps you a lot :)"
+    )
+    arg.add_argument("-v", "--version", action="store_true")
+    arg.add_argument(
+        "-I", "--install", action="store_true", help="force to re-install runs"
+    )
+    arg.add_argument(
+        "-b",
+        "--by",
+        nargs="*",
+        default=False,
+        metavar=",".join(RUNS),
+        help=f'install with/run by, default all installed, eg: `--by={",".join(DEFAULT)}`',
+    )
+    arg.add_argument(
+        "-@",
+        "--at",
+        metavar=CONFIG["search_dir"],
+        help='search_dir of git repos, eg: `--at=".."` if GVHMR is current work dir',
+    )
+    arg.add_argument("-i", "--input", nargs="*", metavar="in.mp4")
+    arg.add_argument("-o", "--outdir", metavar=OUTPUT_DIR, default=OUTPUT_DIR)
+    arg.add_argument(
+        "-r",
+        "--range",
+        metavar="[a,b]or[a,duration]",
+        default="",
+        help="video time range, eg: `--range=0:0:1,0:2` is 1s~2s, `--range=10` is 0s~10s",
+    )
     # arg.add_argument('--euler', action='store_true', help='use euler_XYZ for bones rotations for export data')
     ns, args = arg.parse_known_args()
     return ns, args
 
 
 async def mocap(
-    inputs: list[str] = [], outdir=OUTPUT_DIR,
-    Range='', at=CONFIG['search_dir'], by: Sequence[TYPE_RUNS] = RUNS,
-    args: list[str] = []
+    inputs: list[str] = [],
+    outdir=OUTPUT_DIR,
+    Range="",
+    at=CONFIG["search_dir"],
+    by: Sequence[TYPE_RUNS] = RUNS,
+    args: list[str] = [],
 ):
     if at:
-        CONFIG['search_dir'] = at
-    os.makedirs(CONFIG['search_dir'], exist_ok=True)
+        CONFIG["search_dir"] = at
+    os.makedirs(CONFIG["search_dir"], exist_ok=True)
     os.makedirs(outdir, exist_ok=True)
 
     _by = list(copy.deepcopy(by))
     for i in by:
-        if i and getattr(CONFIG, i, True) and os.path.exists(os.path.join(CONFIG[i], 'pixi.toml')):
+        if (
+            i
+            and getattr(CONFIG, i, True)
+            and os.path.exists(os.path.join(CONFIG[i], "pixi.toml"))
+        ):
             _by.remove(i)
     await install(runs=_by)
     if inputs:
@@ -75,35 +130,38 @@ async def mocap(
             await run(by, i, outdir, Range=Range, args=args)
     else:
         for b in [b for b in by if b in RUNS]:
-            p = await Python(args.pop(0) if args else '', *args, run=b)  # type: ignore
+            p = await Python(args.pop(0) if args else "", *args, run=b)  # type: ignore
 
 
 def script_entry():
     args, _args = argParse()
     if len(sys.argv) <= 1:
         from .server.mcp import main as mcp_main
+
         mcp_main()
         return
     if args.by:
         by = []
         for r in args.by:
-            by += r.split(',')
+            by += r.split(",")
     else:
         by = DEFAULT
 
     if args.install and by and by[0]:  # fix mocap -I -b ''
         for r in by:
-            del CONFIG[r]   # TODO
+            del CONFIG[r]  # TODO
             setattr(CONFIG, r, None)
 
-    asyncio.run(mocap(
-        inputs=args.input,
-        outdir=args.outdir,
-        Range=args.range,
-        at=args.at,
-        by=by,
-        args=_args,
-    ))
+    asyncio.run(
+        mocap(
+            inputs=args.input,
+            outdir=args.outdir,
+            Range=args.range,
+            at=args.at,
+            by=by,
+            args=_args,
+        )
+    )
 
 
 if __name__ == "__main__":
--- src/mocap_wrapper/install/dynhamr.py
+++ src/mocap_wrapper/install/dynhamr.py
@@ -1,59 +1,69 @@
-import re
-from warnings import deprecated
 from .static import gather, i_python_env, git_pull, Git, run_1by1
 from .huggingface import i_hugging_face
 from ..lib import *
+
 Log = getLogger(__name__)
-_STEM = 'dynhamr'
+_STEM = "dynhamr"
 _name_ = RUNS_REPO[_STEM]
 
 
 async def i_dynhamr(Dir: Path | str = CONFIG[_STEM]):
     Log.info(f"📦 Install {_name_}")
-    if not Path(Dir, '.git').exists():
-        await Git(['clone', 'https://github.com/ZhengdiYu/Dyn-HaMR', str(Dir)])
+    if not Path(Dir, ".git").exists():
+        await Git(["clone", "https://github.com/ZhengdiYu/Dyn-HaMR", str(Dir)])
     tasks = [
         i_thirdparty(Dir=Dir),
         i_hugging_face(_STEM),
     ]
-    return await gather(tasks, success_msg=f'Installed {_name_}')
+    return await gather(tasks, success_msg=f"Installed {_name_}")
 
 
 def i_thirdparty(Dir: Path | str = CONFIG[_STEM]):
     queue = [
-        i_python_env(Dir=Dir, pixi_toml=f'{_STEM}.toml', use_mirror=False),
+        i_python_env(Dir=Dir, pixi_toml=f"{_STEM}.toml", use_mirror=False),
         ENV_cuda_toolkit(),
         i_dpvo(),
     ]
-    if not Path(Dir, 'third-party', 'DROID-SLAM', 'setup.py').exists():
+    if not Path(Dir, "third-party", "DROID-SLAM", "setup.py").exists():
         queue.insert(0, git_pull())
     return run_1by1(queue)
 
 
 async def ENV_cuda_toolkit(Dir: Path | str = CONFIG[_STEM]):
-    CUDA_HOME = Path(Dir, '.pixi', 'envs', 'default')
+    CUDA_HOME = Path(Dir, ".pixi", "envs", "default")
     if CUDA_HOME and CUDA_HOME.exists():
-        os.environ['CUDA_HOME'] = str(CUDA_HOME)
-        Log.info(f'{CUDA_HOME=}')
+        os.environ["CUDA_HOME"] = str(CUDA_HOME)
+        Log.info(f"{CUDA_HOME=}")
     else:
-        Log.error('CUDA_HOME not set, please check your pixi installation')
+        Log.error("CUDA_HOME not set, please check your pixi installation")
 
 
-def i_dpvo(setup_py=Path(CONFIG[_STEM], 'third-party', 'DROID-SLAM', 'setup.py')): return Python(setup_py, 'install', run=_STEM)    # '''install DROID-SLAM with CUDA compiling job'''
+def i_dpvo(setup_py=Path(CONFIG[_STEM], "third-party", "DROID-SLAM", "setup.py")):
+    return Python(
+        setup_py, "install", run=_STEM
+    )  # '''install DROID-SLAM with CUDA compiling job'''
 
 
 # @deprecated('You can download *.npy from ...')
-def i_bmc(Dir=Path(CONFIG[_STEM], 'Hand-BMC-pytorch')):
-    '''if *.npy is not ready-made, we build from BMC'''
+def i_bmc(Dir=Path(CONFIG[_STEM], "Hand-BMC-pytorch")):
+    """if *.npy is not ready-made, we build from BMC"""
     if_missing_then_calc = {
-        Path(CONFIG[_STEM], '_DATA', 'BMC', 'bone_len_max.npy'): Python(Dir / 'calculate_bmc.py', run='dynhamr', env='bmc'),
-        Path(CONFIG[_STEM], '_DATA', 'BMC', 'CONVEX_HULLS.npy'): Python(Dir / 'calculate_convex_hull.py', run='dynhamr', env='bmc'),
+        Path(CONFIG[_STEM], "_DATA", "BMC", "bone_len_max.npy"): Python(
+            Dir / "calculate_bmc.py", run="dynhamr", env="bmc"
+        ),
+        Path(CONFIG[_STEM], "_DATA", "BMC", "CONVEX_HULLS.npy"): Python(
+            Dir / "calculate_convex_hull.py", run="dynhamr", env="bmc"
+        ),
     }
     queue = []
     _gather = []
     if not Path(Dir).exists():
-        _gather.append(Git(['clone', 'https://github.com/ZhengdiYu/Dyn-HaMR', str(Dir)]))
-    _gather.append(i_python_env(Dir=Dir, pixi_toml=f'{_STEM}.toml', env='bmc', use_mirror=False))
+        _gather.append(
+            Git(["clone", "https://github.com/ZhengdiYu/Dyn-HaMR", str(Dir)])
+        )
+    _gather.append(
+        i_python_env(Dir=Dir, pixi_toml=f"{_STEM}.toml", env="bmc", use_mirror=False)
+    )
     queue.append(gather(_gather))
     # joints_zip = Path(Dir, 'joints.zip')  # from google drive or baidu pan
     # p = await unzip(joints_zip, Dir)
@@ -64,9 +74,11 @@ def i_bmc(Dir=Path(CONFIG[_STEM], 'Hand-BMC-pytorch')):
             is_missing = True
     if not is_missing:
         queue = []
-    os.chdir(Dir)   # fix: relative open files under BMC/*.npy
+    os.chdir(Dir)  # fix: relative open files under BMC/*.npy
     if is_linux:
-        os.environ['QT_QPA_PLATFORM'] = 'xcb'   # fix: This application failed to start because it could not find or load the Qt platform plugin "wayland" in "". Available platform plugins are: eglfs, minimal, minimalegl, offscreen, vnc, xcb.
+        os.environ["QT_QPA_PLATFORM"] = (
+            "xcb"  # fix: This application failed to start because it could not find or load the Qt platform plugin "wayland" in "". Available platform plugins are: eglfs, minimal, minimalegl, offscreen, vnc, xcb.
+        )
     return run_1by1(queue)
 
 
@@ -81,6 +93,7 @@ def i_bmc(Dir=Path(CONFIG[_STEM], 'Hand-BMC-pytorch')):
 #     return run_1by1(queue)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import asyncio
+
     asyncio.run(i_bmc())
